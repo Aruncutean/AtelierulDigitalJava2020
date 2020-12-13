eade0a9d6e97bbf9677601deccc3e9ea4e9187b8
Aruncutean <aruncutean.raul12@gmail.com>
2020-12-13 17:01:34 +0200

    Challenge 1 2 3 4

--- src/L4/Challenge4/Main.java
+++ src/L4/Challenge4/Main.java
@@ -6,6 +6,7 @@ public class Main {
         Stage1 stage1=new Stage1("./src/L4/Challenge4/input.txt","./src/L4/Challenge4/output.txt");
         stage1.start();
         Stage2 stage2=new Stage2("./src/L4/Challenge4/input.txt","./src/L4/Challenge4/output1.txt");
+
         stage2.start(3,5);
     }
 }
